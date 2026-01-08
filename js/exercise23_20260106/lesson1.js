486e59b9b84d0ea0fd452194e389189e1fe2ea15
nguyentokyo <nguyencaocuong0240@gmail.com>
2026-01-08 21:16:14 +0900

    2026/01/06 complete lesson 1–2

---
+++ js/exercise23_20260106/lesson1.js
@@ -0,0 +1,30 @@
+
+const isTriangle = (a, b, c) => {
+    const isTriangle = (a + b > c) && (b + c > a) && (c + a > b);
+    const isEquilateral = (a === b && b === c);
+    const isIsosceles = (a === b || b === c || a === c);
+    const isRight = (a ** 2 === b ** 2 + c ** 2) || (b ** 2 === a ** 2 + c ** 2) || (c ** 2 === a ** 2 + b ** 2);
+
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+        return "Not a number"
+    } else if (!(isTriangle)) {
+        return "Not a triangle"
+    } else if (isEquilateral) {
+        return "Equilateral triangle";
+    } else if (isIsosceles) {
+        return "Isosceles triangle"
+    } else if (isRight) {
+        return "Right triangle"
+    } else {
+        return "Scalene triangle"
+    }
+}
+
+console.log(isTriangle("1", "4", 2)); // Not a number OK
+console.log(isTriangle(1, 4, 2)); // Not a triangle OK
+console.log(isTriangle(3, 4, 5)); // Right triangle OK
+console.log(isTriangle(5, 5, 5)); // Equilateral triangle OK
+console.log(isTriangle(5, 4, 2)); // Scalene triangle OK
+
+
+
---
+++ js/exercise23_20260106/lesson2.js
@@ -0,0 +1,14 @@
+
+
+function isPerfectSquare(x) {
+    if (typeof x !== 'number') {
+        return false
+    } else {
+        return (x ** 0.5) % 1 === 0;
+    }
+}
+console.log(isPerfectSquare("1"));  //string ⇒ false
+console.log(isPerfectSquare(-1));   //false
+console.log(isPerfectSquare(2));    //false
+console.log(isPerfectSquare(4));    //true
+console.log(isPerfectSquare(25));   //true
